e920ac201f111efcab2ad4180d4ccf0eab098cb8
Bradlee Barnes <69256931+StupidRepo@users.noreply.github.com>
2025-04-06 21:57:39 +0100

    feat: fody frrr

--- Patches/PerformancePatches.cs
+++ Patches/PerformancePatches.cs
@@ -7,16 +7,30 @@ namespace Scheduled.Patches;
 [HarmonyPatch(typeof(UnityEngine.Debug))]
 public class PerformancePatches
 {
-    [HarmonyPatch(nameof(UnityEngine.Debug.LogWarning))]
+    [HarmonyPatch(nameof(UnityEngine.Debug.LogWarning), typeof(object), typeof(UnityEngine.Object))]
     [HarmonyPrefix]
-    public static bool LogWarning()
+    public static bool LogWarning(object message, UnityEngine.Object context)
     {
         return false; // stop spamming the console with warnings
     }
     
-    [HarmonyPatch(nameof(UnityEngine.Debug.LogError))]
-    [HarmonyPostfix]
-    public static bool LogError()
+    [HarmonyPatch(nameof(UnityEngine.Debug.LogError), typeof(object), typeof(UnityEngine.Object))]
+    [HarmonyPrefix]
+    public static bool LogError(object message, UnityEngine.Object context)
+    {
+        return false; // stop spamming the console with errors
+    }
+    
+    [HarmonyPatch(nameof(UnityEngine.Debug.LogWarning), typeof(object), typeof(UnityEngine.Object))]
+    [HarmonyPrefix]
+    public static bool LogWarning(object message)
+    {
+        return false; // stop spamming the console with warnings
+    }
+    
+    [HarmonyPatch(nameof(UnityEngine.Debug.LogError), typeof(object), typeof(UnityEngine.Object))]
+    [HarmonyPrefix]
+    public static bool LogError(object message)
     {
         return false; // stop spamming the console with errors
     }
---
+++ Patches/SupplierConfigPatches.cs
@@ -0,0 +1,36 @@
+﻿using GameKit.Utilities.Types;
+using HarmonyLib;
+using ItemIconCreator;
+using ScheduleOne.DevUtilities;
+using ScheduleOne.Economy;
+using ScheduleOne.Quests;
+using ScheduleOne.StationFramework;
+using ScheduleOne.UI.Compass;
+using UnityEngine;
+
+namespace Scheduled.Patches;
+
+[HarmonyPatch(typeof(SupplierLocationConfiguration))]
+public class SupplierConfigPatches
+{
+    [HarmonyPatch(nameof(SupplierLocationConfiguration.Activate))]
+    [HarmonyPrefix]
+    public static void ActivatePrefix(SupplierLocationConfiguration __instance)
+    {
+        Plugin.Logger.LogInfo($"Activating compass for supplier: {__instance.SupplierID}");
+        CompassManager.Instance.AddElement(
+            __instance.gameObject.transform,
+            Resources.LoadAll<RectTransform>("").First()
+        );
+    }
+
+    [HarmonyPatch(nameof(SupplierLocationConfiguration.Deactivate))]
+    [HarmonyPrefix]
+    public static void DeactivatePrefix(SupplierLocationConfiguration __instance)
+    {
+        Plugin.Logger.LogInfo($"Deactivating compass for supplier: {__instance.SupplierID}");
+        CompassManager.Instance.RemoveElement(
+            __instance.gameObject.transform
+        );
+    }
+}
